0712988cb943f08fc0a3cd553ff4670620d5f28b
SarverCoder <sarvargamer@gmail.com>
2025-05-26 17:25:36 +0500

    Add project files.

---
+++ DiceGameApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using DiceGameApplication.Classes;
+using DiceGameApplication.User;
+
+namespace DiceGameApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!ValidationError.ValidateDiceInput(args, out List<Die> dice))
+            {
+                return;
+            }
+
+            new Game(dice).Start();
+        }
+    }
+}
